f1b7a47b45068a9fc44822ccc7c5a89c7fc72638
Eric Wittle <eric@p21n.com>
2024-03-05 16:33:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# AWS Publish SNS topic action
+
+This action sends a message to an SNS topic.
+
+## Inputs
+
+### `message`
+
+**Required** The message to send
---
+++ index.js
@@ -0,0 +1,28 @@
+// const core = require('@actions/core');
+import { getInput, setFailed } from "@actions/core";
+// const github = require('@actions/github');
+import { fromEnv } from "@aws-sdk/credential-providers";
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
+
+try {
+    const TOPIC_ARN = getInput("TOPIC_ARN") || process.env.TOPIC_ARN;
+    let snsClient = new SNSClient({ credentials: fromEnv() });
+    if (!TOPIC_ARN) {
+        throw new Exception('No TOPIC_ARN defined as input or environment');
+    }
+    let message = `
+Repository: ${process.env.GITHUB_REPOSITORY}
+Branch: ${process.env.GITHUB_REF}
+URL: https://github.com/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}
+Message:
+${getInput("MESSAGE") || 'Testing'}`;
+    const response = await snsClient.send(
+        new PublishCommand( {
+            TopicArn: TOPIC_ARN,
+            Message: message
+        }),
+    );
+    console.log(response);
+} catch (error) {
+    setFailed(error.message);
+}
